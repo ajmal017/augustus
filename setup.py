b80f7dfcc7b0794f51300c91c63bae2dbeee6bb3
Jialue Chen <jialuechen@outlook.com>
2018-10-12 09:11:59 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+
+"""
+Augustus - A event-driven algorithm trading library in Python.
+
+This Project is an open source quantitative trading framework written in Python.
+"""
+
+import os
+
+from setuptools import Command,find_packages,setup
+
+class CleanCommand(Command):
+    user_options=[]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.egg-info")
+
+setup(
+    name="Augustus",
+    version="0.0.1",
+    author="Jialue Chen",
+    author="jialuechen@outlook.com",
+    license="MIT",
+    url="https://github.com/jialuechen/augustus",
+    description="Algorithm trading framework in Python",
+    packages=find_packages(),
+    cmdClass={
+        "clean":CleanCommand,
+    },install_requires=["pandas","numpy","pymongo"]
+)
