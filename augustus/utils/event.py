a13040d0b90f4af5ff58c8ccfc873231af4b5ff2
Jialue Chen <jialuechen@outlook.com>
2018-10-12 19:58:01 -0400

    add event engine in event.py

--- augustus/utils/event.py
+++ augustus/utils/event.py
@@ -1,3 +1,26 @@
 import datetime
 import numpy as np 
 import pandas as pd 
+from queue import Queue,Empty
+
+class EventEngine(object):
+    def __init__(self):
+        self.__queue=Queue()
+        self.__isactive=False
+
+
+    def __run(self):
+        while self.__isactive==True:
+            try:
+                event=self.__queue.get(block=True,timeout=1)
+            except Empty:
+                pass
+
+    def __process(self):
+        pass
+
+    def __begin(self):
+        pass
+
+    def __end(self):
+        pass
