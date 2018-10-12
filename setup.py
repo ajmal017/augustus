194bf75de2eaa7dc01f3de4655dc8baba51bede6
Jialue Chen <jialuechen@outlook.com>
2018-10-12 14:27:09 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name="Augustus",
     version="0.0.1",
     author="Jialue Chen",
-    author="jialuechen@outlook.com",
+    authoremail="jialuechen@outlook.com",
     license="MIT",
     url="https://github.com/jialuechen/augustus",
     description="Algorithm trading framework in Python",
