a9a3264e655f934997a284a5ef27f1dadc3436b6
Jialue Chen <jialuechen@outlook.com>
2018-10-16 16:57:21 -0400

    fix data.py

--- augustus/utils/data.py
+++ augustus/utils/data.py
@@ -3,10 +3,7 @@ import numpy as np
 import pandas as pd 
 
 class Data(self,openDate,closeDate,volume,price):
-    self.opendate=opendate
-    self.closeDate=closeDate
-    self.volume=volume
-    self.price=price
+    
 
     
     
