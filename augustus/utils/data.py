880ff488294e38defbaef7119fd6f32bbe8bd915
Jialue Chen <jialuechen@outlook.com>
2018-10-16 16:53:32 -0400

    set up basic structure of financial data

--- augustus/utils/data.py
+++ augustus/utils/data.py
@@ -1,3 +1,12 @@
 import datetime
 import numpy as np 
 import pandas as pd 
+
+class Data(self,openDate,closeDate,volume,price):
+    self.opendate=opendate
+    self.closeDate=closeDate
+    self.volume=volume
+    self.price=price
+
+    
+    
