12d1d758ab53e40aa1828cec3f0e64682ce6b097
Jialue Chen <jialuechen@outlook.com>
2018-10-18 09:28:35 -0400

    rename some functions

--- augustus/utils/math_tools.py
+++ augustus/utils/math_tools.py
@@ -2,7 +2,7 @@ import math
 from decimal import Decimal
 from numpy import isnan
 
-def check_with_tolerance(a,b,tol=10e-7,is_equal=False):
+def check_is_equal(a,b,atol=10e-7, rtol=10e-7,is_equal=False):
     if is_equal and isnan(a) and isnan(b):
         return True
     return math.fabs(a - b) <= (atol + rtol * math.fabs(b))
