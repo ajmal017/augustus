d956d4a6b73d71886f8d7f11141b900fdb38aaf9
Jialue Chen <jialuechen@outlook.com>
2018-10-19 21:22:17 -0400

    refactor model

--- augustus/assets/model.py
+++ augustus/assets/model.py
@@ -2,12 +2,12 @@ import math
 import datetime
 
 class Model(object):
-    def __init__(self,name,volumn,start,end,price,changerate):
-        self.name=name
-        self.volumn=volumn
-        self.start=start 
-        self.end=end
-        self.price=price
-        self.changerate=changerate
+    def __init__(self,date,open,close,high,low,volume,code):
+        self.date=date
+        self.open=open  
+        self.close=close
+        self.high=high
+        self.low=low 
+        self.code=code
         
 
--- augustus/utils/data.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import numpy as np 
-import pandas as pd 
-
-class Data(self,openDate,closeDate,volume,price):
-
-    
-    
