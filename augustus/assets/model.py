4e7afec09628200cd661886ef46240b5f2a768f0
Jialue Chen <jialuechen@outlook.com>
2018-10-20 22:40:05 -0400

    refractor model

--- augustus/assets/model.py
+++ augustus/assets/model.py
@@ -8,6 +8,28 @@ class Model(object):
         self.close=close
         self.high=high
         self.low=low 
+        self.volume=volume
         self.code=code
+
+    def get_date(self):
+        return self.date
+
+    def get_open(self):
+        return self.open 
+    
+    def get_close(self):
+        return self.close 
+
+    def get_high(self):
+        return self.high
+
+    def get_low(self):
+        return self.low
+
+    def get_volume(self):
+        return self.volume
+
+    def get_code(self):
+        return self.code
         
 
