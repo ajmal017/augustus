a3b723d54adf4331ec001043f22fd5c8fb6836ac
Jialue Chen <jialuechen@outlook.com>
2018-12-31 20:02:26 -0500

    refactoring

--- augustus/assets/model.py
+++ augustus/assets/model.py
@@ -2,29 +2,29 @@ import math
 import datetime
 
 class Model(object):
-    def __init__(self,date,open,close,high,low,volume,code):
+    def __init__(self,date,openingPrice,closingPrice,highest,lowest,volume,code):
         self.date=date
-        self.open=open  
-        self.close=close
-        self.high=high
-        self.low=low 
+        self.openingPrice=openingPrice  
+        self.closingPrice=closingPrice
+        self.highest=highest
+        self.lowest=lowest
         self.volume=volume
         self.code=code
 
     def get_date(self):
         return self.date
 
-    def get_open(self):
-        return self.open 
+    def get_openingPrice(self):
+        return self.openingPrice 
     
-    def get_close(self):
-        return self.close 
+    def get_closingPrice(self):
+        return self.closingPrice 
 
-    def get_high(self):
-        return self.high
+    def get_highest(self):
+        return self.highest
 
-    def get_low(self):
-        return self.low
+    def get_lowest(self):
+        return self.lowest
 
     def get_volume(self):
         return self.volume
